2d98056f68a29384f8279d9d960650751a6ade22
raaynaldo <raaynaldo@hotmail.com>
2020-10-31 22:03:06 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ActiveRecord::Base
   belongs_to :author
   validate :is_title_case 
 
+  before_validation :make_title_case
+  before_save :email_author_about_post
+
   private
 
   def is_title_case
@@ -11,7 +14,12 @@ class Post < ActiveRecord::Base
     end
   end
 
+  def email_author_about_post
+
+  end
+
   def make_title_case
     self.title = self.title.titlecase
   end
+
 end
